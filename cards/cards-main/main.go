488d025b001bf11ef09d335f6d611a0e661be822
Chris Ibraheem <chrisibraheem@Chriss-MacBook-Pro.local>
2021-03-31 17:13:18 -0400

    work on cards-main

--- cards/cards-main/main.go
+++ cards/cards-main/main.go
@@ -1,18 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	// syntax for making a slice
-	cards := []string{newCard(), "Ace of Diamonds"}
+	cards := deck{newCard(), "Ace of Diamonds"}
 	cards = append(cards, "Six of Spades")
 
-	// i = index of this element in the array
-	// card = current card being iterated over
-	// range cards = takes "cards" slice and loops over the range of it
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
