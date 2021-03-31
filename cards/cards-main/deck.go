7be8daf066882124a3903892c0c82b86d3d660df
Chris Ibraheem <chrisibraheem@Chriss-MacBook-Pro.local>
2021-03-31 17:37:09 -0400

    Created newDeck() function and cleaned main.go

--- cards/cards-main/deck.go
+++ cards/cards-main/deck.go
@@ -5,9 +5,22 @@ import "fmt"
 //Create a new type of 'deck' which is a slice of strings.
 type deck []string
 
-//d is a receiver
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	//replace variables you declare but don't use with underscores to bypass Go errors of unused variables.
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+//d is a receiver, a copy of the deck we are using and is called in the function as 'd'
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
