0fd2cfe1855c9997b5d4f5a7e8dc3a70cfb94b2f
Chris Ibraheem <chrisibraheem@Chriss-MacBook-Pro.local>
2021-04-02 14:42:16 -0400

    Added toString method

--- cards/cards-main/main.go
+++ cards/cards-main/main.go
@@ -1,11 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	// syntax for making a slice
-	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 5)
 
 	// cards.print()
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+
+	// greeting := "Hi there!"
+	//[]byte turns a variable into its byte form
+	// fmt.Println([]byte(greeting))
+
+	cards := newDeck()
+	fmt.Println(cards.toString())
 }
