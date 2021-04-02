de5de08a4fc5ea0118bec3130bb9373afaf474d2
Chris Ibraheem <chrisibraheem@Chriss-MacBook-Pro.local>
2021-04-02 18:14:56 -0400

    test functions added

--- cards/cards-main/deck_test.go
+++ cards/cards-main/deck_test.go
@@ -1,11 +1,35 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
-
+	//check card length
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %d", len(d))
+	}
+	//check first card
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card Ace of Spades, but got %v", d[0])
+	}
+	//check last card
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card King of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
 	}
 }
