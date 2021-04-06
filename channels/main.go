fbacaef50317672d33c1a8fa7d12c7f293143aee
Chris-LewisI <chrisibraheem@gmail.com>
2021-04-05 21:47:17 -0400

    channels work

---
+++ channels/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		//checkLink((link))
+		go checkLink(link, c) //make it concurrent (go routines)
+	}
+
+	for l := range c {
+		go func(link string) { //function literal
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link) //this is a blocking call
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up!")
+	c <- link
+}
